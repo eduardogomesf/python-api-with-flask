b11bd4e879245059e27c704882489900232d43bf
Eduardo Gomes <edugomesf@icloud.com>
2024-10-26 08:30:04 -0300

    feat: add abort for treated error

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -19,8 +20,8 @@ def create_store():
 @app.post("/items")
 def create_item():
     item_data = request.get_json()
-    if item_data["store_id"] not in stores: 
-     return { "message": "Store not found" }, 404
+    if item_data["store_id"] not in stores:
+      abort(404, message="Store not found")
     
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
@@ -37,14 +38,14 @@ def get_store(store_id):
     try:
       return stores[store_id]
     except KeyError:
-      return { "message": "Store not found" }, 404
+      abort(404, message="Store not found")
 
 @app.get("/items/<string:id>")
 def get_item_in_stores(item_id):
   try:
     return items[item_id]
   except KeyError:
-    return { "message": "Item not found" }, 404
+    abort(404, message="Item not found")
     
       
 
